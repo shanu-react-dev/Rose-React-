5ae89a465c60439475dc782e50aa936caaa1caa0
shanu-react-dev <amarjeet.kr.dev@gmail.com>
2026-04-05 02:50:27 +0000

    debouncing and throttling

--- ReactJS Topics/debouncing-and-throttling/script.js
+++ ReactJS Topics/debouncing-and-throttling/script.js
@@ -1,12 +1,16 @@
 let normalCount = document.querySelector(".normalCount")
 let throttleCount = document.querySelector(".throttleCount")
 let debounceCount = document.querySelector(".debounceCount")
+let debounceVal = document.querySelector(".debounceVal")
+let normalVal = document.querySelector(".normalVal")
 
 
 let nCount = 0
-let normalFunction = () => {
+let normalFunction = (value) => {
     nCount++
     normalCount.innerHTML = `Normal Count: ${nCount}`
+    normalVal.innerHTML = `Normal Value: ${value}`
+
 }
 
 let tCount = 0
@@ -16,23 +20,32 @@ let throttleFunction = () => {
         tCount++
         isScroll = false
     setTimeout(() => {
-        throttleCount.innerHTML = `Normal Count: ${tCount}`
+        throttleCount.innerHTML = `Throttle Count: ${tCount}`
         isScroll = true
     }, 1000);
     }
 }
 
 let dCount = 0
-let interval;
-let debounceFunction = () => {
-    dCount++
-    clearTimeout(interval)
-    interval = setTimeout(() => {
+let timer;
+let debounceFunction = (val) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+        dCount++
         debounceCount.innerHTML = `Debounce Count: ${dCount}`
+        debounceVal.innerHTML = `Debounce Value: ${val}`
     }, 1000);
 }
+
 let showCount = () => {
     normalFunction()
     throttleFunction()
     debounceFunction()
 }
+
+let input = document.getElementById("searchTerm")
+input.addEventListener("keyup", function (e) {
+    debounceFunction(e.target.value)
+    normalFunction(e.target.value)
+})
+
